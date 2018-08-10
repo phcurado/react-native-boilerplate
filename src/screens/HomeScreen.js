d663583d4a8c4b1e37c5569128180b4feba34af0
phcurado <phcurado@gmail.com>
2018-08-10 00:26:06 -0300

    Added mobx

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { Container, Button, Text } from 'native-base';
 import navigationScreen from '../hoc/navigationScreen';
+import CounterStore from '../Stores/CounterStore';
+import { observer } from 'mobx-react';
+@observer
 class HomeScreen extends Component {
     constructor(props) {
         super(props);
@@ -9,7 +12,14 @@ class HomeScreen extends Component {
         return (
             <Container style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
                 <Button onPress={() => this.props.navigation.navigate('DetailsScreen')}>
-                    <Text>Button</Text>
+                    <Text>Go to Details</Text>
+                    <Text>{CounterStore.counter}</Text>
+                </Button>
+                <Button primary block onPress={() => CounterStore.increment()}>
+                    <Text>Increment</Text>
+                </Button>
+                <Button primary block onPress={() => CounterStore.decrement()}>
+                    <Text>Decrement</Text>
                 </Button>
             </Container>
         );
